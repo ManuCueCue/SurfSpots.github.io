3bd8c4cd3ddb2583a27fef90ae55b8491c1d9c11
ManuCueCue <51972337+ManuCueCue@users.noreply.github.com>
2019-06-18 22:55:38 +0200

    SurfSpots
    Surf Spots Qgis2web

---
+++ data/EscuelasdeSurf_3.js
@@ -0,0 +1,23 @@
+var json_EscuelasdeSurf_3 = {
+"type": "FeatureCollection",
+"name": "EscuelasdeSurf_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Escuela de surf LLanes", "Web": "escueladesurfllanes.surf", "Valor": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -4.753167080579003, 43.417214476566407 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Escuela de surf LLanes Surf y Aventura", "Web": "surfllanes.com", "Valor": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -4.785052760686558, 43.425702555886858 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Escuela Asturiana de Surf", "Web": "escuelaasturianadesurf.com", "Valor": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -4.825607084609292, 43.435163307121847 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Surf Skull LLanes", "Web": "surfskull.es", "Valor": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -4.875832818485042, 43.441878396177124 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Santa Marina Surf Camp", "Web": "santamarinasurfcamp.com", "Valor": 5.0 }, "geometry": { "type": "Point", "coordinates": [ -5.073988730884001, 43.465518109201099 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Caravia Surf School", "Web": null, "Valor": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -5.17661655839602, 43.474377846056882 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Marejada Escuela de Surf", "Web": "escuelamarejada.com", "Valor": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -5.213437371848091, 43.473563484036973 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Surf Camp Ribadesella", "Web": "surfcampribadesella.com", "Valor": 1.0 }, "geometry": { "type": "Point", "coordinates": [ -5.066648854882725, 43.465126945650461 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Rodiles Surf School", "Web": "rodilesurf.com", "Valor": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -5.376330491100501, 43.532987109599134 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "SkoolSurf", "Web": "skoolsurf.com", "Valor": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -5.648202667746037, 43.539962883863325 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Tablas Surf School", "Web": "tablassurfschool.com", "Valor": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -5.655911614864152, 43.540689576615698 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Horizon Escuela De Surf", "Web": "horizonteescueladesurf.com", "Valor": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -5.955570445898602, 43.576698847449343 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Surfcamp las Dunas", "Web": "escueladesurflasdunas.com", "Valor": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -5.962022122663579, 43.574625971276916 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Alma Surf School", "Web": "almasurfschool.com", "Valor": 3.0 }, "geometry": { "type": "Point", "coordinates": [ -6.646857880397808, 43.56514966208988 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Cueva Surf School", "Web": null, "Valor": 2.0 }, "geometry": { "type": "Point", "coordinates": [ -6.471867729727146, 43.549569212864135 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "nombre": "Surf School Occidente", "Web": null, "Valor": 4.0 }, "geometry": { "type": "Point", "coordinates": [ -6.713237351369669, 43.555840908988472 ] } }
+]
+}
---
+++ data/TiendasdeSurf_2.js
@@ -0,0 +1,16 @@
+var json_TiendasdeSurf_2 = {
+"type": "FeatureCollection",
+"name": "TiendasdeSurf_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Sand shop LLanes", "Web": null, "Valor": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -4.752109817186605, 43.419174026347775 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "LLanesport", "Web": null, "Valor": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -4.758189497240902, 43.422582760566108 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Planeta Palombina", "Web": "planetapalombina.com", "Valor": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -4.809627054348725, 43.431071304521204 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Waikiki", "Web": "waikikisurfshop.es", "Valor": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -5.058105033875327, 43.462408344416851 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Tottishop Surf Skate Snow", "Web": "tottishop.es", "Valor": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -5.436330311312658, 43.483577811315129 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Tablas surf shop", "Web": "tablassurfshop.com", "Valor": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -5.655403547801169, 43.540302085090289 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Astur Boards", "Web": "asturboards.com", "Valor": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -5.668121424205935, 43.542054522091597 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Narval wetsuits", "Web": "narvalwetsuits.com", "Valor": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -5.654019106738982, 43.540096028799155 ] } },
+{ "type": "Feature", "properties": { "Id": 0.0, "Nombre": "Slash surfboards", "Web": "slashboards.com", "Valor": 0.0 }, "geometry": { "type": "Point", "coordinates": [ -5.950541328921318, 43.58026333463247 ] } }
+]
+}
